4c11454184b72818753e53543e83ac1a8fbef71f
Kody Peterson <kody@abe.ai>
2019-03-15 14:33:36 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# pino-websocket
+
+This module provides a "transport" for [pino][pino] that simply forwards
+messages to a websocket socket server. The module can echo the received
+logs or work silently.
+
+You should install `pino-websocket` globally for ease of use:
+
+```bash
+$ npm install -g pino-websocket
+```
+
+[pino-websocket]: https://www.npmjs.com/package/pino-websocket
+
+## Usage
+
+Given an application `foo` that logs via [pino][pino], and a webscoket server
+that collects logs on port `5000` on IP `10.10.10.5`, you would use `pino-websocket`
+like so:
+
+```bash
+$ node foo | pino-websocket -a 10.10.10.5 -p 5000
+```
---
+++ app.js
@@ -0,0 +1,54 @@
+const nopt = require('nopt');
+const exitHook = require('async-exit-hook');
+const websocketFactory = require('./lib/websocket');
+let connection;
+let options = {
+    address: '127.0.0.1',
+    port: '514',
+    path: '/',
+    echo: true,
+    reconnect: false,
+    reconnectTries: Infinity,
+};
+const longOpts = {
+    address: String,
+    port: Number,
+    path: String,
+    reconnect: Boolean,
+    reconnectTries: Number,
+    echo: Boolean,
+    help: Boolean,
+    version: Boolean,
+};
+const shortOpts = {
+    a: '--address',
+    p: '--port',
+    pa: '--path',
+    r: '--reconnect',
+    t: '--reconnectTries',
+    ne: '--no-echo',
+    h: '--help',
+    v: '--version',
+};
+const argv = nopt(longOpts, shortOpts, process.argv);
+options = Object.assign(options, argv);
+
+if (options.help) {
+    console.log(fs.readFileSync(path.join(__dirname, 'help.txt'), 'utf8'));
+    process.exit(0);
+}
+
+if (options.version) {
+    console.log('pino-socket', require('./package.json').version);
+    process.exit(0);
+}
+
+connection = websocketFactory(options)
+
+exitHook((callback) => {
+    try {
+        connection.close();
+    } catch (e) {}
+
+    callback();
+})
